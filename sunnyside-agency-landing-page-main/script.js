877515d9b44f0a3f6561c6cdedfee1fd9a14d584
Caetano Smaniotto <smaniottocaetano@gmail.com>
2023-05-02 15:57:23 -0300

    scroll menu
    adição de efeito na navbar ao descer a página

--- sunnyside-agency-landing-page-main/script.js
+++ sunnyside-agency-landing-page-main/script.js
@@ -1,5 +1,16 @@
+/*menu hamburger*/
 var menu = document.getElementById("menu-lista")
 var btn = document.getElementById("menu-hamburger")
 btn.addEventListener("click", function () {
     menu.classList.toggle("shown");
 });
+
+/*mudança de cor da navbar*/
+var navbar = document.querySelector('.navbar')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else if (window.scrollY <= 50) {
+        navbar.classList.remove('scrolled');
+    }
+})
